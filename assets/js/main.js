5bc296a88fb4ab71bdfffef9bbb19b3bf5f8e17d
Amir <loskiq@gmail.com>
2018-04-15 10:55:19 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,15 +20,6 @@
 		var	$window = $(window),
 			$body = $('body');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
-
 		// Mobile?
 			if (skel.vars.mobile)
 				$body.addClass('is-mobile');
